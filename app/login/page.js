055b082d4580422a5d971556ba67c266a00cdb59
prajjvaljaiswal <prajjaiswal158@gmail.com>
2024-11-12 00:20:31 +0530

    Light changes

--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Login from '../../components/Login'
+import LoginForm from '@/components/LoginForm'
+import Header from '@/components/header'
 
 const page = () => {
   return (
-   <Login/>
+  //  <Login/>
+    <div>
+    <Header/>
+    <LoginForm/>
+    </div>
   )
 }
 
